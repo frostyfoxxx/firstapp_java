0869a47a46806880fdb04992d67e2f05167a8fee
frostyfoxxx <frosty.fox14@yandex.ru>
2021-03-13 17:17:35 +0400

    New commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        string hello = ;
-	    System.out.println("Hello World");
+        String hello = "Hello";
+	    System.out.println(hello + ", World");
         System.out.println("Hello World");
         System.out.println("Hello World");
     }
